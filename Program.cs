ac42662d95d097a746148b53eb3350582b38bdd3
mikhail.madoyan <mikhail.madoyan@gmail.com>
2022-12-15 22:43:59 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class
+            Person tom = new Person("Mike", 32);
+
+            string personName = tom.name;
+            int personAge = tom.age;
+            Console.WriteLine($"Name: {personName} Age: {personAge}");
+
+            tom.name = "Tom";
+            tom.age = 37;
+
+            tom.Print();
+        }
+
+        class Person
+        {
+            public string name = "Undefined";
+            public int age;
+
+            public Person(string n, int a)
+            {
+                name = n;
+                age = a;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Name: {name} Age: {age}");
+            }
+        }
+    }
+}
